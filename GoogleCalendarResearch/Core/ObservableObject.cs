b429ef717c7ee9d7430cdc08355f84c8c5aacce0
Micah Bonzet <micah@farsoft.co.za>
2023-12-18 14:37:36 +0200

    Separated Observable object from observable window

--- GoogleCalendarResearch/Core/ObservableObject.cs
+++ GoogleCalendarResearch/Core/ObservableObject.cs
@@ -5,6 +5,16 @@ using System.Windows;
 
 namespace GoogleCalendarResearch.Core;
 
+public class ObservableObject : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
 public class ObservableWindow : MetroWindow, INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
